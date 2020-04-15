71887ea35b87d593f75b2e56ccbb585d385789eb
Geoff St. Pierre <serundeputy@gmail.com>
2020-04-14 20:10:44 -0400

    Check if symlink exists.

--- sql-dump-sanitize.php
+++ sql-dump-sanitize.php
@@ -74,7 +74,9 @@ $nice_name = $sanitize ? "$db_name-$date-sanatized" : "$db_name-$date";
 exec("mv $backup_destination/$file_name.sql.gz $backup_destination/$nice_name.sql.gz");
 if ($latest) {
   if (file_exists("$backup_destination/$db_name-latest.sql.gz")) {
-    unlink("$backup_destination/$db_name-latest.sql.gz");
+    if (is_link("$backup_destination/$db_name-latest.sql.gz")) {
+      unlink("$backup_destination/$db_name-latest.sql.gz");
+    }
   }
   symlink("$backup_destination/$file_name.sql.gz", "$backup_destination/$db_name-latest.sql.gz");
 }
